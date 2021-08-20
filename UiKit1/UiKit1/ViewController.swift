01f3316340d33e3ad45bcfb9d428cea70ea89b62
Gustavo Braghin <gugabraghin@gmail.com>
2021-08-20 15:09:56 -0300

    Added new button

--- UiKit1/UiKit1/ViewController.swift
+++ UiKit1/UiKit1/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var buttonStoryBoard: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
